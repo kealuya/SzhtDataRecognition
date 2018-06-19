74fb3795889a5791e0f80a3c4933cab3b32ae034
kealuya <kealuya@126.com>
2018-06-20 01:08:22 +0800

    生产版本

--- routes/AiOpenJsonMaker.js
+++ routes/AiOpenJsonMaker.js
@@ -28,6 +28,38 @@ var AiOpenJsonMaker_ocr_generalocr = (promise) => (res) => {
     });
 }
 
+var AiOpenJsonMaker_ocr_generalocr_getJson = (promise) => (res) => {
+
+    promise.then(function (sres) {
+        var returnJson = JSON.parse(sres.text);
+        if (returnJson.ret == 0) {
+            //success
+            var item_list = returnJson.data.item_list;
+            // {'姓':0.998497,'名':0.99587}
+            var itemInfoArray = item_list.map(function (val, ind, arr) {
+                var itemString = val.itemstring;
+                var itemConfidence = {};
+                val.words.forEach(v => {
+                    itemConfidence[v.character] = v.confidence;
+                });
+                return {
+                    'itemString': itemString,
+                    'itemConfidenceObj': itemConfidence
+                };
+            });
+            console.log(itemInfoArray);
+            res.render('result',{'returnJson':JSON.stringify(itemInfoArray)});
+        } else {
+            //error
+            res.JSON({ error_code: returnJson.ret, msg: returnJson.msg });
+        }
+    }).catch(function(err){
+        res.JSON(err);
+    });
+}
+
+
 module.exports = {
-    'AiOpenJsonMaker_ocr_generalocr': AiOpenJsonMaker_ocr_generalocr
+    'AiOpenJsonMaker_ocr_generalocr': AiOpenJsonMaker_ocr_generalocr,
+    'AiOpenJsonMaker_ocr_generalocr_getJson':AiOpenJsonMaker_ocr_generalocr_getJson
 }
--- routes/ocr_generalocr.js
+++ routes/ocr_generalocr.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+var multiparty = require('multiparty');
 const aojm = require('./AiOpenJsonMaker');
 const taoa = require('./TencentAiOpenApi');
 
 router.get('/test', function (req, res, next) {
-    var bitmap = fs.readFileSync('routes/搜狗截图17年11月25日1512_1.png');
+    var bitmap = fs.readFileSync('D:/浩天差旅数据识别/szhtDataRecognition/routes/test.png');
     var img_base64 = new Buffer(bitmap).toString('base64');
     aojm.AiOpenJsonMaker_ocr_generalocr(taoa.ocrGeneralocr(img_base64))(res);
 });
@@ -13,6 +14,22 @@ router.get('/test', function (req, res, next) {
 router.post('/ocr_generalocr', function (req, res, next) {
     var imgbase64 = req.body.imgbase64;
     aojm.AiOpenJsonMaker_ocr_generalocr(taoa.ocrGeneralocr(imgbase64))(res);
+});
+
+router.post('/ocr_generalocr_from_page', function (req, res, next) {
+
+    var dfe = req.body;
+    var dfaef = req.files;
+    var form = new multiparty.Form();
+
+    form.parse(req, function(err, fields, files) {
+        //fields:非文件内容；files：文件内容
+        console.log(files.upfile[0].originalFilename);
+        console.log(files.upfile[0].path);
+        var bitmap = fs.readFileSync(files.upfile[0].path);
+        var img_base64 = new Buffer(bitmap).toString('base64');
+        aojm.AiOpenJsonMaker_ocr_generalocr_getJson(taoa.ocrGeneralocr(img_base64))(res);
+    });
 });
 
 module.exports = router;
